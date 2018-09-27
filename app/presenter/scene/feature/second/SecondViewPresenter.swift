bd694c448a383c05924b4bef73b8a4bc0dd3c828
Guilherme Cherubini <guilherme.acherubini@gmail.com>
2018-09-27 18:46:29 -0300

    improving second view presenter

--- app/presenter/scene/feature/second/SecondViewPresenter.swift
+++ app/presenter/scene/feature/second/SecondViewPresenter.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class SecondViewPresenter {
     
-    var view: SecondView!
-    var service: PersistenceService!
+    let view: SecondView
+    let service: PersistenceService
     var nameFromParam: String?
     
     init(service: PersistenceService, view: SecondView, nameFromParam: String?) {
@@ -27,21 +27,21 @@ class SecondViewPresenter {
     
     func presentNameFromParam() {
         guard let nameFromParam = nameFromParam, !nameFromParam.isEmpty else {
-            view?.showError(with: "Error getting user name from param")
+            view.showError(with: "Error getting user name from param")
             return
         }
         
         let message = "\(nameFromParam) (from param)"
-        view?.setNameFromParam(message)
+        view.setNameFromParam(message)
     }
     
     func presentNameFromPersistence() {
         guard let nameFromPersistence = service.getUserName(), !nameFromPersistence.isEmpty else {
-            view?.showError(with: "Error getting user name from persistence")
+            view.showError(with: "Error getting user name from persistence")
             return
         }
         
         let message = "\(nameFromPersistence) (from persistence)"
-        view?.setNameFromPersistence(message)
+        view.setNameFromPersistence(message)
     }
 }
